f6ab2f160d98b0c4c6ff46135f01c6809eb0df7f
EC2 Default User <ec2-user@ip-172-31-38-43.ec2.internal>
2020-09-20 12:32:38 +0000

    3-7

---
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'e90719fb6a8c7a9e8c2c74aef599e0f4a92aaff97596af986a58ed10f7c8bfc434eb5bea48a69bbd9285e732561e90113b9b8b0f67466708c16c8c9ea541b2e1'
