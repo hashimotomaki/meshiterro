867efff1c8062f83a6f10b61fcd2d9eab52aa7a6
EC2 Default User <ec2-user@ip-172-31-35-64.ec2.internal>
2020-09-27 15:56:06 +0000

    add bookcontroller

--- Bookers2/app/controllers/books_controller.rb
+++ Bookers2/app/controllers/books_controller.rb
@@ -31,7 +31,11 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
-    @user = @book.user
+    if @book.user == current_user
+      render action: :edit
+    else
+      redirect_to books_path
+    end
   end
 
   def update
